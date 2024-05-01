f9ed6c6c723fc3d7c91ff332bc6744beb9735740
olivia leese <116760201+ol287@users.noreply.github.com>
2024-05-01 10:57:48 +0100

    Update Week 1.c

--- Week 1.c
+++ Week 1.c
@@ -1,8 +1,14 @@
 // question 1
 
+// defines a macro
+//replaces the header line with the contents of stdio.h
+//preprocess looks in the file path it expects to find the stdio.h in
 #include <stdio.h>
 
+//by convention main is always at the top
+// must always return 0
 int main() {
+    //sends an ouput to stdout stream
     printf("Hello, World!\n");
     return 0;
 }
@@ -12,10 +18,15 @@ int main() {
 #include <stdio.h>
 
 int main() {
+    
+    //short int size 2 bytes
     unsigned short int x = 1;
     short int y = 65535;
+
+    //char size is one byte
     unsigned char c = 'A';
 
+    //sends an output to stdout
     printf("x has the value %hu\n", x);
     printf("y has the value %d\n", y);
     printf("c has the value %c\n", c);
@@ -30,6 +41,7 @@ int main() {
 int main() {
     int i;
     for (i = 1; i <= 20; i++) {
+        //remainder
         if (i % 2 == 0) {
             printf("%d\n", i);
         }
@@ -47,6 +59,7 @@ int main() {
         if (i % 2 == 0) {
             printf("%d\n", i);
         }
+        //increment
         i++;
     }
     return 0;
